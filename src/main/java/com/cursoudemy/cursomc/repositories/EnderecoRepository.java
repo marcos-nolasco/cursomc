54933359176ea2397a6538ed68019ab9686dea58
Marcos Nolasco <manolasco@gmail.com>
2022-03-22 06:36:02 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/cursoudemy/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.cursoudemy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursoudemy.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
